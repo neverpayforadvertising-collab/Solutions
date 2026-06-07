7993ff8855cf09d4780277cd751a1bd4cc428b6b
kevin <neverpayforadvertising@gmail.com>
2026-06-08 08:46:19 +0900

    added validation

--- Django/core/users/serializers.py
+++ Django/core/users/serializers.py
@@ -5,3 +5,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+        
+    def validate_email(self, value):
+        if "@" not in value:
+            raise serializers.ValidationError("Invalid email")
+        return value
+            
