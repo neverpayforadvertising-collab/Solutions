692df8f58773e8dd166ea5bdb5796f5d103716d7
kevin <neverpayforadvertising@gmail.com>
2026-06-08 09:24:13 +0900

    updated on 08_06_2026

--- Django/core/core/urls.py
+++ Django/core/core/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
-from users.views import get_users
+from django.urls import path, include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('users/', get_users),
-    
+    path('', include('users.urls')),
 ]
+
