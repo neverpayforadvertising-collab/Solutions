43e418116a29c917b4cc09d08ed2b9220fa447e9
kevin <neverpayforadvertising@gmail.com>
2026-06-08 09:23:20 +0900

    refactored with modelviewset

--- Django/core/users/views.py
+++ Django/core/users/views.py
@@ -1,20 +1,25 @@
 # users/views.py
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import UserSerializer
 
-@api_view(['GET', 'POST'])
-def get_users(request):
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# @api_view(['GET', 'POST'])
+# def get_users(request):
     
-    if request.method == 'GET':
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
+#     if request.method == 'GET':
+#         users = User.objects.all()
+#         serializer = UserSerializer(users, many=True)
+#         return Response(serializer.data)
     
-    if request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+#     if request.method == 'POST':
+#         serializer = UserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
